7c1148f128536657190697c8facc7b5cffbe9213
sabo <saysabou@gmail.com>
2026-03-06 23:02:40 +0100

    Refactor Timeslot as a readonly Value Object with typed properties

--- src/Domain/Reservation/Timeslot.php
+++ src/Domain/Reservation/Timeslot.php
@@ -4,14 +4,13 @@ declare(strict_types=1);
 
 namespace App\Domain\Reservation;
 
-class Timeslot
-{
-    public $start;
-    public $end;
+use DateTimeImmutable;
 
-    public function __construct($start, $end)
-    {
-        $this->start = $start;
-        $this->end = $end;
+final readonly class Timeslot
+{
+    public function __construct(
+        public DateTimeImmutable $start,
+        public DateTimeImmutable $end,
+    ) {
     }
 }
